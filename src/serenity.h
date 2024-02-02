a4eae23b3a7152150f18640973c8308fe085678e
d0pelrh <jsshscifi@gmail.com>
2024-02-03 00:08:30 +0530

    map size selection

--- src/serenity.h
+++ src/serenity.h
@@ -3,3 +3,7 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct{	float i,j,k;	} vect3f;
+
+typedef struct{ int h,w;	} Map;
--- src/start.c
+++ src/start.c
@@ -1,5 +1,7 @@
 #include "serenity.h"
 void clear_screen(int cp);
+vect3f hue_selection(void);
+Map* mapsize_selection(void);
 
 
 int main(int ac, char** av){
@@ -12,49 +14,12 @@ while (getline(&l,&n,f)!=-1)
 	printw("%s",l);
 free(l); fclose(f); getch();
 
-for (int cp=20,i=1000;cp<30;cp++,i-=30){
-	init_color(cp,i,i,i);
-	init_pair(cp,0,cp);
-	init_pair(100+cp,cp,0);}
-for (int cp=30,i=1000,j=1000;cp<40;cp++,i-=10,j-=20){
-	init_color(cp,1000,i,j);
-	init_pair(cp,0,cp);
-	init_pair(100+cp,cp,0);}
-for (int cp=40,i=1000;cp<50;cp++,i-=25){
-	init_color(cp,i,i,1000);
-	init_pair(cp,0,cp);
-	init_pair(100+cp,cp,0);}
-WINDOW* whue =newwin(7,30,(LINES-7)/2,(COLS-30)/2);
-char c=0; int hl =20; do {
-switch(c) {
-	case 'a':	if (hl%10)   hl--;	break;
-	case 'd':	if (hl%10<9) hl++;	break;
-	case 'w':	if (hl>19)   hl-=10;	break;
-	case 's':	if (hl<40)   hl+=10;	break;
-	default:				break;}
-clear_screen(hl); refresh();
-wattron(whue,COLOR_PAIR(100+hl)); box(whue,0,0);
-mvwprintw(whue,0,(30-15)/2,"pls correct hue");
-mvwprintw(whue,6,(30-18)/2,"wasd - 1 to select");
-wmove(whue,2,5);
-for (int i=20;i<30;i++){
-	wattron(whue,COLOR_PAIR(i));
-	if (i==hl) { waddch(whue, '.'); waddch(whue, '.');}
-	else { waddch(whue,' '); waddch(whue,' ');}}
-wmove(whue,3,5);
-for (int i=30;i<40;i++){
-	wattron(whue,COLOR_PAIR(i));
-	if (i==hl) { waddch(whue, '.'); waddch(whue, '.');}
-	else { waddch(whue,' '); waddch(whue,' ');}}
-wmove(whue,4,5);
-for (int i=40;i<50;i++){
-	wattron(whue,COLOR_PAIR(i));
-	if (i==hl) { waddch(whue, '.'); waddch(whue, '.');}
-	else { waddch(whue,' '); waddch(whue,' ');}}
-wrefresh(whue);
-} while ((c=getch())!='1');
+vect3f hue =hue_selection();
+Map* map =mapsize_selection();
+getch();
 
 endwin();	return 0;}
+
 
 
 void clear_screen(int cp){
@@ -62,3 +27,74 @@ move(0,0); attron(COLOR_PAIR(cp));
 for (int y=0;y<LINES;y++)
 	for (int x=0;x<COLS;x++)
 		addch(' ');	return;}
+
+
+vect3f hue_selection(void){
+for (int cp=20,i=1000;cp<30;cp++,i-=30){
+	init_color(cp,i,i,i);
+	init_pair(cp,COLOR_BLACK,cp);
+	init_pair(100+cp,cp,COLOR_BLACK);}
+for (int cp=30,i=1000,j=1000;cp<40;cp++,i-=10,j-=20){
+	init_color(cp,1000,i,j);
+	init_pair(cp,COLOR_BLACK,cp);
+	init_pair(100+cp,cp,COLOR_BLACK);}
+for (int cp=40,i=1000;cp<50;cp++,i-=25){
+	init_color(cp,i,i,1000);
+	init_pair(cp,COLOR_BLACK,cp);
+	init_pair(100+cp,cp,COLOR_BLACK);}
+WINDOW* whue =newwin(7,30,(LINES-7)/2,(COLS-30)/2);
+char c=0; int hl =20; do {
+switch (c) { case 'a':	if (hl%10)   hl--;	break;
+	     case 'd':	if (hl%10<9) hl++;	break;
+	     case 'w':	if (hl>29)   hl-=10;	break;
+	     case 's':	if (hl<40)   hl+=10;	break;
+	     default:				break;}
+clear_screen(hl); refresh();
+wattron(whue,COLOR_PAIR(100+hl)); box(whue,0,0);
+mvwprintw(whue,0,(30-15)/2,"pls correct hue");
+mvwprintw(whue,6,(30-18)/2,"wasd - 1 to select");
+wmove(whue,2,5); for (int i=20;i<30;i++){
+	wattron(whue,COLOR_PAIR(i));
+	if (i==hl){ waddch(whue, '.'); waddch(whue, '.');}
+	else { waddch(whue,' '); waddch(whue,' ');}}
+wmove(whue,3,5); for (int i=30;i<40;i++){
+	wattron(whue,COLOR_PAIR(i));
+	if (i==hl){ waddch(whue, '.'); waddch(whue, '.');}
+	else { waddch(whue,' '); waddch(whue,' ');}}
+wmove(whue,4,5); for (int i=40;i<50;i++){
+	wattron(whue,COLOR_PAIR(i));
+	if (i==hl){ waddch(whue, '.'); waddch(whue, '.');}
+	else { waddch(whue,' '); waddch(whue,' ');}}
+wrefresh(whue); } while ((c=getch())!='1'); delwin(whue);
+vect3f hue;
+if (hl>40){	int i =1000-hl%10*25;
+		init_color(21,i,i,1000);
+		hue =(vect3f){i/1000.0,i/1000.0,1};}
+else if (hl>30){ int i =1000-hl%10*10; int j =1000-hl%10*20;
+		init_color(21,1000,i,j);
+		hue =(vect3f){1,i/1000.0,j/1000.0};}
+else {		int i =1000-hl%10*30;
+		init_color(21,i,i,i);
+		hue =(vect3f){i/1000.0,i/1000.0,i/1000.0};}
+init_pair(1,21,COLOR_BLACK);
+init_pair(2,COLOR_BLACK,21);	return hue;}
+
+
+Map* mapsize_selection(void){
+WINDOW* wmap =newwin(9,30,(LINES-9)/2,(COLS-30)/2);
+wattron(wmap, COLOR_PAIR(1)); box(wmap,0,0);
+mvwprintw(wmap,0,(30-15)/2,"choose map size");
+mvwprintw(wmap,2,3,"1. pocket");
+mvwprintw(wmap,3,3,"2. small");
+mvwprintw(wmap,4,3,"3. normal");
+mvwprintw(wmap,5,3,"4. large");
+mvwprintw(wmap,6,3,"5. xtra large");
+wrefresh(wmap);
+char c; while ((c=getch())<'1'||c>'5');
+Map* map =malloc(sizeof(Map));
+switch (c) { case '1':	map->h=50;  map->w=100;	break;
+	     case '2':	map->h=100; map->w=200;	break;
+	     case '3':	map->h=150; map->w=300;	break;
+	     case '4':	map->h=200; map->w=400;	break;
+	     case '5':	map->h=250; map->w=500; break;
+	     default:				break;}	return map;}
