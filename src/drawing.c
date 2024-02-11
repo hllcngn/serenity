6494d163400b3474756c3a0067c652effca4a342
d0pelrh <jsshscifi@gmail.com>
2024-02-11 21:25:27 +0530

    refactored drawing.c/display_map()

--- src/drawing.c
+++ src/drawing.c
@@ -1,36 +1,37 @@
 #include "serenity.h"
 
 void display_map(Map* map, vect2i pos){
-attron(COLOR_PAIR(CP_BASE));
-int y=0;
-for (y; pos.y-WGAMEH/2+y<0; y++){
-	move(WGAMEY+y,WGAMEX);
+vect2i wgame =(vect2i){(LINES-WGAMEH)/2,(COLS-WGAMEW)/2};
+vect2i cam   =(vect2i){pos.y-WGAMEH/2,  pos.x-WGAMEW/2};
+int y=0; attron(COLOR_PAIR(CP_BASE));
+for (y; cam.y+y<0; y++){
+	move(wgame.y+y,wgame.x);
 	for (int x=0;x<WGAMEW;x++) addch(' ');}
-for (y; pos.y-WGAMEH/2+y<map->h && y<WGAMEH; y++){
-	move(WGAMEY+y,WGAMEX);
-	int x=0;
-	for (x; pos.x-WGAMEW/2+x<0; x++) addch(' ');
-	for (x; pos.x-WGAMEW/2+x<map->w && x<WGAMEW; x++)
-	if	(map->fg[pos.y-WGAMEH/2+y][pos.x-WGAMEW/2+x])
-		addch(map->fg[pos.y-WGAMEH/2+y][pos.x-WGAMEW/2+x]);
-	else if	(map->clsn[pos.y-WGAMEH/2+y][pos.x-WGAMEW/2+x])
-		addch(map->clsn[pos.y-WGAMEH/2+y][pos.x-WGAMEW/2+x]);
-	else	addch(map->bg[pos.y-WGAMEH/2+y][pos.x-WGAMEW/2+x]);
-	for (x; x<WGAMEW; x++) addch(' ');}
+for (y; cam.y+y<map->h &&y<WGAMEH; y++){
+	int x=0; move(wgame.y+y,wgame.x);
+	for (x; cam.x+x<0; x++)    addch(' ');
+	for (x; cam.x+x<map->w &&x<WGAMEW; x++)
+		if	(map->fg[cam.y+y][cam.x+x])
+			addch(map->fg[cam.y+y][cam.x+x]);
+		else if	(map->clsn[cam.y+y][cam.x+x])
+			addch(map->clsn[cam.y+y][cam.x+x]);
+		else	addch(map->bg[cam.y+y][cam.x+x]);
+	for (x; x<WGAMEW; x++)     addch(' ');}
 for (y; y<WGAMEH; y++){
-	move(WGAMEY+y,WGAMEX);
+	move(wgame.y+y,wgame.x);
 	for (int x=0;x<WGAMEW;x++) addch(' ');}
 
-for (Instance* it=map->inst;it;it=it->next)
-if     (it->y+it->inter->h>pos.y-WGAMEH/2 &&it->y<pos.y+WGAMEH/2
-      &&it->x+it->inter->w>pos.x-WGAMEW/2 &&it->x<pos.x+WGAMEW/2){
-int yy =it->y<pos.y-WGAMEH/2? pos.y-WGAMEH/2-it->y :0;
-for (yy;yy<it->inter->h &&it->y+yy<pos.y+WGAMEH/2;yy++){
-int xx =it->x<pos.x-WGAMEW/2? pos.x-WGAMEW/2-it->x :0;
-for (xx;xx<it->inter->w &&it->x+xx<pos.x+WGAMEW/2;xx++){
-	move(   WGAMEY+WGAMEH-((pos.y+WGAMEH/2)-it->y)+yy,
-		WGAMEX+WGAMEW-((pos.x+WGAMEW/2)-it->x)+xx);
-	if (it->inter->map[yy][xx]!=' ') addch(it->inter->map[yy][xx]);}}}
+for (Instance* in=map->inst;in;in=in->next)
+if     (in->y+in->inter->h>cam.y &&in->y<cam.y+WGAMEH
+      &&in->x+in->inter->w>cam.x &&in->x<cam.x+WGAMEW){
+int yy =in->y<cam.y? cam.y-in->y :0;
+for (yy; yy<in->inter->h &&in->y+yy<cam.y+WGAMEH; yy++){
+	int xx =in->x<cam.x? cam.x-in->x :0;
+	for (xx; xx<in->inter->w &&in->x+xx<cam.x+WGAMEW; xx++){
+		move(   wgame.y+WGAMEH-(cam.y+WGAMEH-in->y)+yy,
+			wgame.x+WGAMEW-(cam.x+WGAMEW-in->x)+xx);
+		if (in->inter->map[yy][xx]!=' ')
+			addch(in->inter->map[yy][xx]);}}}
 return;}
 
 
