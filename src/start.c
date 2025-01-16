65482812da9a4e248d6af370b495a0b01a05d65a
hllcngn99 <realhllcngn@gmail.com>
2025-01-16 18:02:00 +0100

    starting codebase review and cleaning

--- src/start.c
+++ src/start.c
@@ -1,4 +1,5 @@
 #include "serenity.h"
+void title_screen(void);
 void new_game(vect3f*, Map**, int*, Player**, int);
 vect3f hue_selection(int);
 Map* mapsize_selection(int);
@@ -7,30 +8,37 @@ void set_names(Map*, Player*, int);
 
 
 int main(int ac, char** av){
-initscr(); cbreak(); noecho(); curs_set(0);
-start_color(); refresh();
+// init
+initscr(); cbreak(); noecho(); curs_set(0); start_color(); refresh();
 srand(time(NULL));
 
-FILE* f =fopen("ass/title.txt","r");
-char* l =NULL; size_t n;
-int i=0; while (getline(&l,&n,f)!=-1){
-	mvprintw((LINES-7)/2+i,(COLS-100)/2,"%s",l); i++;}
-free(l); fclose(f); getch();
+title_screen();
 
+// new game
 vect3f hue; Map* map; int difficulty; Player* pl;
 new_game(&hue, &map, &difficulty, &pl, 1);
 
+// loading assets
 Info *info =malloc(sizeof(Info));
 info->action =create_actiontable();
 info->interactive =create_intertable(info->action);
-//mvprintw(0,0,"create_map\n"); getch();
+
 create_map(map,info);
 
 game(hue, map, pl, info);
 
 free_actiontable(info->action); free_intertable(info->interactive);
-free_player(pl); free_map(map);
-endwin();	return 0;}
+free_player(pl); free_map(map); endwin();	return 0;}
+
+
+
+void title_screen(){
+FILE* f =fopen("ass/title.txt","r");
+char* l =NULL; size_t n;
+int i=0; while (getline(&l,&n,f)!=-1){
+	mvprintw((LINES-7)/2+i,(COLS-100)/2,"%s",l); i++;}
+	//these numbers correspond to the size of the boxed title text
+free(l); fclose(f); getch();}
 
 
 
