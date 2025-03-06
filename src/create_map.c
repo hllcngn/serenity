99a5ce5e01370c89dedcce34cc0ef0e138b66d96
hllcngn <realhllcngn@gmail.com>
2025-03-06 11:10:13 +0100

    ragged map edges

--- src/create_map.c
+++ src/create_map.c
@@ -7,13 +7,25 @@ map->fg   =calloc_arrayint2(map->h,map->w);
 map->it   =calloc_arrayint2(map->h,map->w);
 map->inst =NULL;
 
-for (int y=0;y<map->h;y++)
-	if (!(y%2)) for (int x=0;x<map->w;x++)
-		if (!(x%2)) map->bg[y][x]='=';
-		else map->bg[y][x]=' ';
-	else for (int x=0;x<map->w;x++)
-		if (!(x%2)) map->bg[y][x]=' ';
-		else map->bg[y][x]='=';
+for (int y=0; y<map->h; y++)
+	if (!(y%2))
+		for (int x=0; x<map->w; x++)
+			if (!(x%2))	map->bg[y][x]='=';
+			else		map->bg[y][x]=' ';
+	else	for (int x=0; x<map->w; x++)
+			if (!(x%2))	map->bg[y][x]=' ';
+			else		map->bg[y][x]='=';
+
+map->bg[0][0]=' '; map->bg[0][map->w-1]=' ';
+map->bg[map->h-1][0]=' '; map->bg[map->h-1][map->w-1]=' ';
+for (int x=2; x<map->w-2; x+=2)
+	if (!(rand()%2)) map->bg[0][x]=' ';
+for (int x=map->h%2+1; x<map->w-2; x+=2)
+	if (!(rand()%2)) map->bg[map->h-1][x]=' ';
+for (int y=2; y<map->h-2; y+=2)
+	if (!(rand()%2)) map->bg[y][0]=' ';
+for (int y=map->w%2+1; y<map->h-2; y+=2)
+	if (!(rand()%2)) map->bg[y][map->w-1]=' ';
 
 Asset* atree1 =load_asset("ass/tree1.txt");
 paste_asset(map,10,10,atree1);
@@ -21,14 +33,16 @@ free_asset(atree1);
 
 add_inst(map,20,20,info->interactive[0]);
 add_inst(map,20,30,info->interactive[0]);
-for (int i=0;i<map->w/10;i++)
+
+for (int i=0;i<map->w/3;i++)
 	add_inst(map,
 		rand()%(map->h-20)+10,rand()%(map->w-20)+10,
 		info->interactive[1]);
-for (int i=0;i<map->w/10/2;i++)
+for (int i=0;i<map->w/6;i++)
 	add_inst(map,
 		rand()%(map->h-20)+10,rand()%(map->w-20)+10,
 		info->interactive[2]);}
+
 
 void free_map(Map* map){
 for (int y=0;y<map->h;y++) free(map->bg[y]);
